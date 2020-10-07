945c6df67fa3695ee42e751641edb11b07abb687
feng <2839597752@qq.com>
2020-10-07 10:45:51 +0800

    完成了登录和注册的功能

---
+++ assets/js/login.js
@@ -0,0 +1,82 @@
+$(function () {
+  //点击“去注册账号的链接”
+  $('#link_reg').on('click', function () {
+    $('.login-box').hide();
+    $('.reg-box').show();
+  })
+
+  //点击“去登录的链接”
+  $('#link_login').on('click', function () {
+    $('.login-box').show();
+    $('.reg-box').hide();
+  })
+  // 从layui 中获取form 对象
+  var form = layui.form;
+  var layer = layui.layer;
+  // 通过from.verify()  函数自定义校验规则
+  form.verify({
+    // 自定义了一个叫做pwd 校验规则
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    // 校验两次密码是否一致的规则
+    repwd: function (value) {
+      // 通过形参拿到的是确认密码框中的内容
+      // 还需要拿到密码框中的内容
+      // 然后进行一次等于的判断
+      // 如果判断失败  则return一个提示消息即可
+      var pwd = $('.reg-box [name=password]').val();
+      if (pwd !== value) {
+        return '两次密码不一致';
+      }
+    }
+  })
+
+  // 监听注册表单的提交事件
+  $('#form_reg').on('submit', function (e) {
+    // 阻止默认的提交行为
+    e.preventDefault();
+    // 发起ajax的post请求
+    var data = {
+      username: $('#form_reg [name=username]').val(),
+      password: $('#form_reg [name=password]').val(),
+    }
+    $.post('/api/reguser', data, function (res) {
+      if (res.status !== 0) return layer.msg(res.message);
+
+      layer.msg('注册成功，请登录！');
+      // 模拟人的行为
+      $('#link_login').click();
+    })
+
+  });
+
+  // 监听登录表单的提交事件
+  // $('#form_login').on('submit', function (e) {
+    $('#form_login').submit(function(e) {
+    // 阻止默认的提交行为
+    e.preventDefault();
+    $.ajax({
+      url: '/api/login',
+      method: 'POST',
+      // 快速获取表单中的数据
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) return layer.msg(res.message);
+        layer.msg('登陆成功');
+        // 将登陆成功得到的token字符串  保存到localStorage 中
+        localStorage.setItem('token', res.token);
+        // 跳转到后台主页
+        location.href = '/index.html';
+      }
+    })
+
+  });
+
+  // 注意： 每次调用$.get() 或 $.post() 或 $.ajax() 的时候
+  // 会先调用ajaxPrefilter 这个函数
+  // 在这个函数中  可以拿到我们给ajax提供的配置对象
+  // $.ajaxPrefilter(function (options) {
+  //   // 在发起真正的ajax请求之前  统一拼接请求的根路经
+  //   options.url = 'http://ajax.frontend.itheima.net' + options.url;
+  // })
+
+})

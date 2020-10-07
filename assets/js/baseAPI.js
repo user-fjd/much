2ec69d2f22b09a1ae74669bcb2da796239712ec9
feng <2839597752@qq.com>
2020-10-07 16:47:18 +0800

    完成了推送

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -10,4 +10,16 @@ $.ajaxPrefilter(function (options) {
       Authorization: localStorage.getItem('token') || ''
     }
   }
+  // 不论成功还是失败  最终都会调用complete 回调函数
+  options.complete = function (res) {
+    // console.log('执行了complete回调：');
+    // console.log(res);
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      // 1.清空本地存储中的token
+      localStorage.removeItem('token');
+      // 2.重新跳转到登录页面
+      location.href = '/login.html';
+    }
+  }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,7 @@ $(function () {
             // 2.重新跳转到登录页面
             location.href = '/login.html';
 
-            
+
             layer.close(index);
         });
     })
@@ -22,6 +22,7 @@ function getUserInfo() {
     $.ajax({
         method: 'GET',
         url: '/my/userinfo',
+        // headers 就是请求头配置对象
         // headers: {
         //     Authorization: localStorage.getItem('token') || ''
         // },
@@ -31,23 +32,32 @@ function getUserInfo() {
             }
             // 调用renderAvatar 渲染用户的头像
             renderAvatar(res.data);
-        }
+        },
+        
+       
     })
 }
 
-// 渲染用户的头像
+/**
+ * 
+ * @param {*} user 存储了用户的信息
+ */
 function renderAvatar(user) {
-    // 1.获取用户的名称
+    // 1.获取用户的名称  短路运算
     var name = user.nickname || user.username;
     // 2.设置欢迎的文本
     $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
     // 3.按需渲染用户的头像
     if (user.user_pic !== null) {
-        // 3.1渲染图片头像
+        //渲染图片头像
+        // attr('属性')  获取属性
+        // attr('属性','属性值');   设置属性
+        // attr('属性',function(){
+        // });
         $('.layui-nav-img').attr('src', user.user_pic).show();
         $('.text-avatar').hide();
     } else {
-        // 3.2 渲染 文本头像
+        //渲染文本头像
         $('.layui-nav-img').hide();
         var first = name[0].toUpperCase();
         $('.text-avatar').html(first).show();
